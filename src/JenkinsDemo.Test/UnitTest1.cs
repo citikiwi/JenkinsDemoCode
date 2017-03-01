259db05943593e19f7f75d0e9fdbd63134abccf6
citikiwi <kenwu.sh@gmail.com>
2017-03-01 09:19:04 +0800

    Initial add all files

---
+++ src/JenkinsDemo.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JenkinsDemo;
+
+namespace JenkinsDemo.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mw = new MyWork();
+            Assert.AreEqual(mw.DoSomething(1), 1);
+            Assert.AreEqual(mw.DoSomething(100), 100);
+        }
+    }
+}
---
+++ src/JenkinsDemo/MyWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo
+{
+    public class MyWork
+    {
+        public int DoSomething(int x)
+        {
+            return x;
+        }
+    }
+}
